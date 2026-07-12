c301ee53e7afa4c4820b52019d090c9cae4989bf
Advik M <advik.mahajan121802@gmail.com>
2026-07-12 21:39:10 +0530

    Adding a loop so that user can play as many times as they want to
    Added input validation and game loop for guessing.

--- number_guessing_game /number_guessing_game.py
+++ number_guessing_game /number_guessing_game.py
@@ -1,3 +1,35 @@
+import random 
+def get_input():
+    while True:
+        try:
+            guess = int(input("Enter Your Guess: "))
+            if 0 <= guess <= 100:
+                return guess
+            else:
+                print("Invalid Input.")
+        except ValueError:
+            print("Invalid Input.")
+def play_game():
+    random_number = random.randint(1, 100)
+    print("The random number chosen is between 0 and 100, both included.")
+    number_of_guesses = 0
+    while True:
+        x = get_input()
+        number_of_guesses += 1
+        if x > random_number:
+            print("Too High!")
+        elif x < random_number:
+            print("Too Low!")
+        else:
+            print("Congratulations!, You Guessed Right!")
+            print(f"You guessed in {number_of_guesses} attempts.")
+            break
+while True:
+    play_game()
+    play_again = input("Do you want to play again? (yes/no): ").lower().strip()
+    if play_again != "yes":
+        print("Thanks for playing!")
+        break
 import random
 random_number = random.randint(1, 100)
 print("The random number chosen is between 0 and 100 (both included). ")
