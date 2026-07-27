7113ce41bbc10571941a40fc1d54ba91b736e4c6
Advik M <advik.mahajan121802@gmail.com>
2026-07-27 06:02:29 +0530

    Update rock_paper_scissor.py

--- rock_paper_scissor_game /rock_paper_scissor.py
+++ rock_paper_scissor_game /rock_paper_scissor.py
@@ -1,1 +1,25 @@
+import random
+while True:
+    computer_choice = random.choice(("rock", "paper", "scissor"))
+    while True:
+        user_choice = input("Enter Your Move: ").lower().strip()
+        if user_choice not in ("rock", "paper", "scissor"):
+            print("Enter a Valid Choice (rock, paper or scissor")
+            continue
+        break
+    print(f"You Chose - {user_choice}")
+    print(f"Computer Chose - {computer_choice}")
+    if user_choice == computer_choice:
+        print("Its a Tie")
+    elif user_choice == "paper" and computer_choice == "rock":
+        print("You Win!")
+    elif user_choice == "scissor" and computer_choice == "paper":
+        print("You Win!")
+    elif user_choice == "rock" and computer_choice == "scissor":
+        print("You Win!")
+    else:
+        print("You Lose!")
+    question = input("Do You Want to Play Again? (yes/no): ").lower().strip()
+    if question != "yes":
+        break
 
